d146d47b88cb6f2d37a77370e75e91c3cf974eec
Camden Cheek <camden@ccheek.com>
2022-08-08 07:50:23 -0600

    move optimization to its own file

--- src/search/mod.rs
+++ src/search/mod.rs
@@ -22,7 +22,10 @@ use crate::shard::{
 
 use self::regex::{extract_regex_literals, ConcatLiteralSet, ExtractedRegexLiterals, LiteralSet};
 
+mod optimize;
 pub mod regex;
+
+use optimize::optimize_extracted;
 
 #[derive(Clone)]
 pub struct DocMatch {
@@ -528,99 +531,6 @@ where
     ) -> ParallelMap<'a, 'scope, T, R, I, F> {
         ParallelMap::new(self, scope, max_parallel, f)
     }
-}
-
-fn optimize_extracted(
-    extracted: ExtractedRegexLiterals,
-    trigrams: &Arc<CompressedTrigramPointers>,
-) -> ExtractedRegexLiterals {
-    use ExtractedRegexLiterals::*;
-
-    match extracted {
-        // We don't handle exact matches specially right now,
-        // so just optimize it as inexact.
-        // Exact(set) => optimize_inexact_literals(vec![set], trigrams),
-        // TODO make sure exact doesn't get too long.
-        Exact(set) => Exact(set),
-        Inexact(sets) => optimize_inexact_literals(sets, trigrams),
-        None => None,
-    }
-}
-
-fn optimize_inexact_literals(
-    sets: Vec<ConcatLiteralSet>,
-    trigrams: &Arc<CompressedTrigramPointers>,
-) -> ExtractedRegexLiterals {
-    let mut sets: Vec<ConcatLiteralSet> = sets
-        .into_iter()
-        .map(|set| optimize_prefix_range_set(set, &trigrams))
-        .flatten()
-        .filter(|set| set.selectivity(&trigrams) < 0.0001)
-        .collect();
-
-    if sets.len() == 0 {
-        return ExtractedRegexLiterals::None;
-    }
-
-    sets.sort_by(|a, b| {
-        a.selectivity(&trigrams)
-            .partial_cmp(&b.selectivity(&trigrams))
-            .unwrap()
-            .reverse()
-    });
-    sets.truncate(3);
-    ExtractedRegexLiterals::Inexact(sets)
-}
-
-fn optimize_prefix_range_set(
-    set: ConcatLiteralSet,
-    trigrams: &Arc<CompressedTrigramPointers>,
-) -> Vec<ConcatLiteralSet> {
-    let max_len = 256;
-    let total_len = set.cardinality();
-    if total_len < max_len {
-        return vec![set];
-    }
-
-    let lits = set.sets();
-    let mut res = Vec::new();
-    for num_slices in 2..lits.len() {
-        res.clear();
-        let target_product = (total_len as f64).powf(1f64 / num_slices as f64) as usize;
-        if target_product > max_len {
-            continue;
-        }
-
-        let mut start = 0;
-        for slice_num in 0..num_slices - 1 {
-            for end in start + 1..lits.len() {
-                let slice_len: usize = lits[start..end]
-                    .iter()
-                    .map(LiteralSet::cardinality)
-                    .product();
-                if slice_len > target_product {
-                    res.push(lits[start..end - 1].to_vec());
-                    start = end - 1;
-                    break;
-                }
-            }
-        }
-        let last_slice = &lits[start..];
-        if last_slice
-            .iter()
-            .map(LiteralSet::cardinality)
-            .product::<usize>()
-            > max_len
-        {
-            continue;
-        }
-        res.push(last_slice.to_vec());
-        return res.into_iter().map(ConcatLiteralSet::new).collect();
-    }
-
-    // I'm not 100% convinced the above loop will always terminate, so as
-    // a safety measure, always just return the original if optimization fails.
-    vec![set]
 }
 
 #[cfg(test)]
