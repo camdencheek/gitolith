50cdbb2517ae16df9e6b6804e4dc93ddcdf57c22
Camden Cheek <camden@ccheek.com>
2022-08-08 13:13:32 -0600

    simplify optimize

--- src/search/optimize.rs
+++ src/search/optimize.rs
@@ -24,28 +24,15 @@ pub fn optimize_extracted(
     }
 }
 
+// TODO take selectivity into account?
 fn optimize_exact_literals(concat: ConcatLiteralSet) -> OptimizedLiterals {
     let cardinality_limit = 2048; // TODO tune this parameter
 
     for n in 1..=concat.as_ref().len() {
-        let split = {
-            let base_len = concat.as_ref().len() / n;
-            let num_with_bonus = concat.as_ref().len() % n;
-            let mut v = Vec::with_capacity(n);
-            let mut start = 0;
-            for i in 0..n {
-                let len = if i < num_with_bonus {
-                    base_len + 1
-                } else {
-                    base_len
-                };
-                v.push(ConcatLiteralSet::new(
-                    concat.as_ref()[start..start + len].to_vec(),
-                ));
-                start = start + len;
-            }
-            v
-        };
+        let split = split_mostly_even(concat.as_ref(), n)
+            .iter()
+            .map(|lits| ConcatLiteralSet::new(lits.to_vec()))
+            .collect::<Vec<_>>();
 
         let split_cardinality = split
             .iter()
@@ -60,6 +47,53 @@ fn optimize_exact_literals(concat: ConcatLiteralSet) -> OptimizedLiterals {
     return optimize_inexact_literals(vec![concat]);
 }
 
-fn optimize_inexact_literals(sets: Vec<ConcatLiteralSet>) -> OptimizedLiterals {
-    todo!()
+fn optimize_inexact_literals(concats: Vec<ConcatLiteralSet>) -> OptimizedLiterals {
+    let mut res = Vec::with_capacity(concats.len());
+    for concat in concats {
+        if let Some(mut split) = optimize_inexact_literal(concat) {
+            res.append(&mut split);
+        } else {
+            return OptimizedLiterals::None;
+        }
+    }
+    OptimizedLiterals::Inexact(res)
+}
+
+fn optimize_inexact_literal(concat: ConcatLiteralSet) -> Option<Vec<ConcatLiteralSet>> {
+    let cardinality_limit = 2048; // TODO tune this parameter
+
+    for n in 1..=concat.as_ref().len() {
+        let split = split_mostly_even(concat.as_ref(), n)
+            .iter()
+            .map(|lits| ConcatLiteralSet::new(lits.to_vec()))
+            .collect::<Vec<_>>();
+
+        let split_cardinality = split
+            .iter()
+            .map(ConcatLiteralSet::cardinality)
+            .sum::<usize>();
+
+        if split_cardinality < cardinality_limit * n {
+            return Some(split);
+        }
+    }
+
+    None
+}
+
+fn split_mostly_even<T>(items: &[T], parts: usize) -> Vec<&[T]> {
+    let base_len = items.len() / parts;
+    let num_with_bonus = items.len() % parts;
+    let mut v = Vec::with_capacity(parts);
+    let mut start = 0;
+    for i in 0..parts {
+        let len = if i < num_with_bonus {
+            base_len + 1
+        } else {
+            base_len
+        };
+        v.push(&items[start..start + len]);
+        start = start + len;
+    }
+    v
 }
