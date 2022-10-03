b32b84e0fc4e9f58e5f4980c136c981340f18268
Camden Cheek <camden@ccheek.com>
2022-10-02 23:17:25 -0600

    add sections

--- src/shard/mod.rs
+++ src/shard/mod.rs
@@ -13,7 +13,7 @@ use super::shard::suffix::SuffixArrayStore;
 use content::ContentStore;
 use derive_more::{Add, From, Into, Sub};
 use docs::DocStore;
-use std::io::Write;
+use std::io::{Read, Write};
 use std::sync::Arc;
 
 #[derive(Copy, Clone, From, Into, Add, Sub, PartialEq, Eq, Hash, Debug)]
@@ -122,5 +122,95 @@ impl Default for ShardHeader {
             sa_ptr: 0,
             sa_len: 0,
         }
+    }
+}
+
+// SimpleSection describes a simple range of bytes.
+#[derive(Clone, Debug, Default)]
+pub struct SimpleSection {
+    pub offset: u64,
+    pub len: u64,
+}
+
+impl ReadWriteStream for SimpleSection {
+    fn write_to<W: Write>(&self, w: &mut W) -> Result<usize, Error> {
+        let mut n = 0;
+        n += self.offset.write_to(w)?;
+        n += self.len.write_to(w)?;
+        Ok(n)
+    }
+
+    fn read_from<R: Read>(r: &mut R) -> Result<Self, Error> {
+        let offset = u64::read_from(r)?;
+        let len = u64::read_from(r)?;
+        Ok(Self { offset, len })
+    }
+}
+
+// CompoundSection describes a range of bytes that contains variable-width items.
+#[derive(Clone, Debug, Default)]
+pub struct CompoundSection {
+    pub data: SimpleSection,
+    pub offsets: SimpleSection,
+}
+
+impl CompoundSection {
+    fn offset(&self) -> u64 {
+        self.data.offset
+    }
+
+    fn len(&self) -> u64 {
+        self.data.len + self.offsets.len
+    }
+
+    fn end(&self) -> u64 {
+        self.offset() + self.len()
+    }
+}
+
+impl ReadWriteStream for CompoundSection {
+    fn write_to<W: Write>(&self, w: &mut W) -> Result<usize, Error> {
+        let mut n = self.data.write_to(w)?;
+        n += self.offsets.write_to(w)?;
+        Ok(n)
+    }
+
+    fn read_from<R: Read>(r: &mut R) -> Result<Self, Error> {
+        let data = SimpleSection::read_from(r)?;
+        let offsets = SimpleSection::read_from(r)?;
+        Ok(Self { data, offsets })
+    }
+}
+
+pub trait ReadWriteStream {
+    fn write_to<W: Write>(&self, w: &mut W) -> Result<usize, Error>;
+    fn read_from<R: Read>(r: &mut R) -> Result<Self, Error>
+    where
+        Self: Sized;
+}
+
+impl ReadWriteStream for u64 {
+    fn read_from<R: Read>(r: &mut R) -> Result<Self, Error> {
+        let mut buf = [0u8; 8];
+        r.read_exact(&mut buf)?;
+        Ok(u64::from_le_bytes(buf))
+    }
+
+    fn write_to<W: Write>(&self, w: &mut W) -> Result<usize, Error> {
+        w.write_all(&self.to_le_bytes())?;
+        Ok(8)
+    }
+}
+
+impl ReadWriteStream for u32 {
+    fn read_from<R: Read>(r: &mut R) -> Result<Self, Error> {
+        let mut buf = [0u8; 4];
+        r.read_exact(&mut buf)?;
+        Ok(u32::from_le_bytes(buf))
+    }
+
+    fn write_to<W: Write>(&self, w: &mut W) -> Result<usize, Error> {
+        w.write_all(&self.to_le_bytes())?;
+        Ok(4)
     }
 }
